d1ae75b0ff151d9322d174787696310f357a1b75
Владислав Куличков <vlad.kulichkov.06@mail.ru>
2026-05-13 23:44:45 +0300

    feat(API): add EntityFramework.Design and configure Program.cs

--- DocFlow.API/Program.cs
+++ DocFlow.API/Program.cs
@@ -1,16 +1,78 @@
-var builder = WebApplication.CreateBuilder(args);
+using DocFlow.Infrastructure.Persistence.DbContexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi;
 
-// Add services to the container.
+namespace DocFlow.API
+{
+	public class Program
+	{
+		public static async Task Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
 
-var app = builder.Build();
+			builder.Services.AddDbContext<AppDbContext>(options =>
+				options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
+			);
+			ConfigureServices(builder);
+
+			var app = builder.Build();
+
+			await ConfigureMiddleware(app);
+
+			app.Run();
+		}
+
+		private static void ConfigureServices(WebApplicationBuilder builder)
+		{
+			var services = builder.Services;
+
+			//services.AddControllers()
+			//	.AddJsonOptions(options =>
+			//	{
+			//		options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+			//	});
 
 
-app.UseHttpsRedirection();
 
-app.UseAuthorization();
+			services.AddControllers();
+			services.AddEndpointsApiExplorer();
 
-app.MapControllers();
+			services.AddSwaggerGen(options =>
+			{
+				options.SwaggerDoc("", new OpenApiInfo
+				{
+					Title = "Doc Flow API",
+				});
+			});
+		}
 
-app.Run();
+		private static async Task ConfigureMiddleware(WebApplication app)
+		{
+			using var scope = app.Services.CreateScope();
+			var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+			await db.Database.MigrateAsync();
+
+			var version = "v1";
+
+			app.UseSwagger();
+
+			app.UseSwaggerUI(options =>
+			{
+				options.SwaggerEndpoint(
+					$"/swagger/{version}/swagger.json",
+					$"Sports Stats API {version}");
+
+				options.RoutePrefix = "swagger";
+			});
+
+
+			app.UseDefaultFiles();
+			app.UseStaticFiles();
+
+			app.UseHttpsRedirection();
+			app.UseAuthorization();
+			app.MapControllers();
+		}
+	}
+}
