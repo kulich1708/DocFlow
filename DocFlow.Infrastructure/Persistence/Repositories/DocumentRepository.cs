b2c5b9d7d3c60228b7244b75cca6ec95e7af247b
Владислав Куличков <vlad.kulichkov.06@mail.ru>
2026-05-14 17:47:58 +0300

    feat(infrastructure): add DocumentRepository

---
+++ DocFlow.Infrastructure/Persistence/Repositories/DocumentRepository.cs
@@ -0,0 +1,22 @@
+﻿using DocFlow.Domain.Documents;
+using DocFlow.Infrastructure.Persistence.DbContexts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DocFlow.Infrastructure.Persistence.Repositories
+{
+	public class DocumentRepository(AppDbContext context)
+	{
+		private readonly AppDbContext _context = context;
+
+		public async Task<Document?> GetAsync(int id)
+			=> await _context.Documents.FirstOrDefaultAsync(d => d.Id == id);
+		public async Task<List<Document>> GetByCategoriesAsync(List<int> categoriesId)
+			=> await _context.Documents.Where(d => categoriesId.Contains(d.CategoryId)).ToListAsync();
+
+		public async Task<List<Document>> GetByUserAsync(int userId)
+			=> await _context.Documents.Where(d => d.AuthorId == userId).ToListAsync();
+	}
+}
