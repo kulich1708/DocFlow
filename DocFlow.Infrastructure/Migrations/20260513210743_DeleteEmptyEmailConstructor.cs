50df1af7b5bcd8f286e462373d9e52055b1a2d51
Владислав Куличков <vlad.kulichkov.06@mail.ru>
2026-05-14 17:47:26 +0300

    chore(infrastructure): add empty migration

---
+++ DocFlow.Infrastructure/Migrations/20260513210743_DeleteEmptyEmailConstructor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DocFlow.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class DeleteEmptyEmailConstructor : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
