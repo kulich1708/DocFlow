390ffbb2262bccde2c47eabe5992feb62433d557
Владислав Куличков <vlad.kulichkov.06@mail.ru>
2026-05-14 17:25:04 +0300

    feat(infrastructure): add UserRepository

---
+++ DocFlow.Infrastructure/Persistence/Repositories/UserRepository.cs
@@ -0,0 +1,20 @@
+﻿using DocFlow.Domain.Users;
+using DocFlow.Infrastructure.Persistence.DbContexts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DocFlow.Infrastructure.Persistence.Repositories
+{
+	public class UserRepository(AppDbContext context)
+	{
+		private readonly AppDbContext _context = context;
+
+		public async Task<List<User>> GetAllAsync()
+			=> await _context.Users.ToListAsync();
+		public async Task<User?> GetAsync(int id)
+			=> await _context.Users.FirstOrDefaultAsync(u => u.Id == id);
+
+	}
+}
