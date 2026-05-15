813dbb03b598c3134a3bbb1a4d2f2c1eef8e913f
Владислав Куличков <vlad.kulichkov.06@mail.ru>
2026-05-15 21:48:01 +0300

    feat: add DTOs and Mappers for users and documents

--- DocFlow.API/App/DTOs/ResponsesDTO.cs
+++ DocFlow.API/App/DTOs/ResponsesDTO.cs
@@ -1,4 +1,20 @@
 ﻿namespace DocFlow.API.App.DTOs
 {
-	public record UserDTO(int Id, string Name, string Surname, string Email);
+	public record UserDTO(
+		int Id, string Name, string Surname, string Email);
+
+	public record DocumentGeneralInfoDTO(
+		int Id, string Name, int? AuthorId, int CategoryId, bool IsPrivate);
+
+	public record DocumentVersionDTO(
+		int Id, int Version, string Content);
+
+	public record DocumentDraftDTO(
+		string Content, DateTime ModifiedAt);
+
+	public record DocumentDTO(
+		DocumentGeneralInfoDTO GeneralInfo,
+		List<DocumentVersionDTO> Versions,
+		DocumentDraftDTO Draft,
+		bool CanEdit);
 }
--- DocFlow.API/App/Mappers/Mapper.cs
+++ DocFlow.API/App/Mappers/Mapper.cs
@@ -1,4 +1,5 @@
 ﻿using DocFlow.API.App.DTOs;
+using DocFlow.API.Documents;
 using DocFlow.API.Users;
 
 namespace DocFlow.API.App.Mappers
@@ -7,5 +8,19 @@ namespace DocFlow.API.App.Mappers
 	{
 		public static UserDTO ToDTO(User user)
 			=> new(user.Id, user.Name, user.Surname, user.Email.Value);
+
+		public static DocumentGeneralInfoDTO ToDocumentGeneralInfoDTO(Document document)
+			=> new(document.Id, document.Name, document.AuthorId,
+				document.CategoryId, document.IsPrivate);
+		public static DocumentVersionDTO ToDTO(DocumentVersion version)
+			=> new(version.Id, version.Version, version.Content.Value);
+		public static DocumentDraftDTO ToDTO(DocumentDraft draft)
+			=> new(draft.Content.Value, draft.ModifiedAt);
+		public static DocumentDTO ToDTO(Document document)
+			=> new(
+				ToDocumentGeneralInfoDTO(document),
+				document.Versions.Select(ToDTO).ToList(),
+				ToDTO(document.Draft),
+				false);
 	}
 }
