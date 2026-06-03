d591a3a5c14d43011a9fb076f9406d8741940745
Владислав Куличков <vlad.kulichkov.06@mail.ru>
2026-06-03 21:44:12 +0300

    feat: add document-list component

---
+++ client/src/components/document-list.tsx
@@ -0,0 +1,14 @@
+import type { DocumentGeneralInfoDTO } from "../api/api-client"
+import { DocumentCard } from "./document-card"
+import "./document-list.scss"
+
+type DocumentListProps = {
+	documents: DocumentGeneralInfoDTO[]
+}
+export function DocumentList({ documents }: DocumentListProps) {
+	return (
+		<div className="document-grid">
+			{documents.map(d => <DocumentCard key={d.id} document={d} />)}
+		</div>
+	)
+}
