d814090244921d90d53d839f28976db3f3620b25
Владислав Куличков <vlad.kulichkov.06@mail.ru>
2026-06-04 16:33:04 +0300

    feat: add new document button to header

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -1,11 +1,14 @@
+import { useState } from "react";
 import { Link, NavLink, useLocation } from "react-router-dom";
 import { getUserIdFromToken } from "../utils/get-user-id-from-token";
+import { Modal } from "./modal/modal";
 import "./header.scss"
 
 export function Header() {
 	const location = useLocation();
 	const isLoggedIn = getUserIdFromToken(localStorage.getItem("token")) !== null;
 	const isCabinetActive = location.pathname.startsWith("/cabinet");
+	const [isAuthModalOpen, setIsAuthModalOpen] = useState(false);
 
 	return (
 		<header className="header">
@@ -21,27 +24,61 @@ export function Header() {
 					>
 						Главная
 					</NavLink>
-					{isLoggedIn ? (
+					{isLoggedIn && (
 						<Link
 							to="/cabinet"
 							className={`header__link${isCabinetActive ? " header__link_active" : ""}`}
 						>
 							Кабинет
 						</Link>
+					)}
+					{!isLoggedIn && (
+						<NavLink
+							to="/login"
+							className={({ isActive }) =>
+								`header__link${isActive ? " header__link_active" : ""}`
+							}
+						>
+							Вход
+						</NavLink>
+					)}
+					{isLoggedIn ? (
+						<Link to="/documents/create" className="header__button">
+							Создать документ
+						</Link>
 					) : (
-						<>
-							<NavLink
-								to="/login"
-								className={({ isActive }) =>
-									`header__link${isActive ? " header__link_active" : ""}`
-								}
-							>
-								Вход
-							</NavLink>
-						</>
+						<button
+							type="button"
+							className="header__button"
+							onClick={() => setIsAuthModalOpen(true)}
+						>
+							Создать документ
+						</button>
 					)}
 				</nav>
 			</div>
+
+			<Modal isOpen={isAuthModalOpen} onClose={() => setIsAuthModalOpen(false)}>
+				<p className="header__modal-text">
+					Создание документов доступно только зарегистрированным пользователям.
+				</p>
+				<div className="header__modal-links">
+					<Link
+						to="/login"
+						className="header__button"
+						onClick={() => setIsAuthModalOpen(false)}
+					>
+						Вход
+					</Link>
+					<Link
+						to="/register"
+						className="header__button"
+						onClick={() => setIsAuthModalOpen(false)}
+					>
+						Регистрация
+					</Link>
+				</div>
+			</Modal>
 		</header>
 	);
 }
